4d929c5b0a6e0ae846b132fa5c0dff8e49495cae
ADKW97 <adam.kwidzinski97@gmail.com>
2020-12-28 19:36:00 +0100

    wersja z firebase

--- app/src/main/java/com/example/mapy/MapsActivity.java
+++ app/src/main/java/com/example/mapy/MapsActivity.java
@@ -12,6 +12,15 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,10 +40,12 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     DatabaseReference reff;
+    private static final String JSON_URL = "https://api.thingspeak.com/channels/1246079/feeds.json?results=2";
     private GoogleMap mMap;
     LocationManager locationManager;
     Marker marker,markerDog;
     Point point=new Point();
+    LatLng coords,coordsDog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +56,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        final Button button =findViewById(R.id.you);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mMap.setMaxZoomPreference(20);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.0f));
+            }
+        });
+        final Button button1 =findViewById(R.id.yourdog);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mMap.setMaxZoomPreference(20);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordsDog, 12.0f));
+            }
+        });
+
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -55,7 +82,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){ //(działa)
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
@@ -68,23 +95,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                 geocoder.getFromLocation(yourX, yourY, 1);
                         //String result = addresses.get(0).getLocality()+":";
                         //result += addresses.get(0).getCountryName();   nazwa lokajci + kraj: chwilowo niepotrzebne-wyswietla sie "you"
-                        LatLng coords = new LatLng(yourX, yourY);
+                        coords = new LatLng(yourX, yourY);
                         if (marker != null){
                             marker.remove();
-                            marker = mMap.addMarker(new MarkerOptions().position(coords).title("You"));   //uaktualnianie pozycji markera
-                            mMap.setMaxZoomPreference(20);
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.0f));
+                            marker = mMap.addMarker(new MarkerOptions().position(coords).title("You"));
+
                         }
                         else{
                             marker = mMap.addMarker(new MarkerOptions().position(coords).title("You"));
+                            mMap.setMinZoomPreference(5);   //zeby nie oddalac na caly swiat?
                             mMap.setMaxZoomPreference(20);
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 21.0f));  //pierwszy marker
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.0f));  //pierwszy marker
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
 
                 }
+
 
                 @Override
                 public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -104,7 +132,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
         else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-            //zmiana czestotliwosci odswiezania w zaleznosc od systansu i czasu ponizej
+            //zmiana czestotliwosci odswiezania w zaleznosc od systansu i czasu ponizej (nie dziala, korzysta z pierwszego)
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
@@ -120,11 +148,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     if (marker != null){
                         marker.remove();
                         marker = mMap.addMarker(new MarkerOptions().position(coords).title("You"));   //uaktualnianie pozycji markera
-                        mMap.setMaxZoomPreference(20);
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.0f));
                     }
                     else{
                         marker = mMap.addMarker(new MarkerOptions().position(coords).title("You"));
+                        //mMap.setMinZoomPreference(5);   //zeby nie oddalac na caly swiat?
                         mMap.setMaxZoomPreference(20);
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.0f));  //pierwszy marker
                     }
@@ -158,20 +185,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 point.y=(double)dataSnapshot.child("longitude").getValue();
                 //Geocoder geocoder= new Geocoder(getApplicationContext());
 
-                LatLng coordsDog = new LatLng(point.x, point.y);
+                coordsDog = new LatLng(point.x, point.y);
                 if (markerDog != null){
                     markerDog.remove();
                     markerDog = mMap.addMarker(new MarkerOptions().position(coordsDog).title("Your Dog"));   //uaktualnianie pozycji markera
-                    //mMap.setMaxZoomPreference(20);
-                    //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordsDog, 12.0f));
+
                 }
                 else{
                     markerDog = mMap.addMarker(new MarkerOptions().position(coordsDog).title("Your Dog"));
-                    //mMap.setMaxZoomPreference(20);
-                    //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordsDog, 21.0f));  //zakomentowane=kamera skierowana na uzytkowniku
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordsDog, 12.0f));  //zakomentowane=kamera skierowana na uzytkowniku
                 }
 
             }
+
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -180,6 +206,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
 
 
+    }
+
+    private void fetchData() {
+        String lightApi = "https://api.thingspeak.com/channels/1246079/fields/1.json?results=2";
+        JsonObjectRequest objectRequest =new JsonObjectRequest(Request.Method.GET, lightApi, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray feeds = response.getJSONArray("feeds");
+                            for(int i=0; i<feeds.length();i++){
+                                JSONObject jo = feeds.getJSONObject(i);
+                                String l=jo.getString("lokalizacja");
+                                point.x= Double.parseDouble(l);
+                                coordsDog = new LatLng(point.x, point.y);
+                                if (markerDog != null){
+                                    markerDog.remove();
+                                    markerDog = mMap.addMarker(new MarkerOptions().position(coordsDog).title("Your Dog"));   //uaktualnianie pozycji markera
+
+                                }
+                                else{
+                                    markerDog = mMap.addMarker(new MarkerOptions().position(coordsDog).title("Your Dog"));
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordsDog, 12.0f));  //zakomentowane=kamera skierowana na uzytkowniku
+                                }
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/mapy/Point.java
+++ app/src/main/java/com/example/mapy/Point.java
@@ -31,6 +31,7 @@ public class Point {
         x=a;
         y=b;
     }
+
     public Point(){
 
     }
